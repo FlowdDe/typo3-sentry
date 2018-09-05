fdbb227eb1676d264aa938b4ce76af1944477724
Felix Tscheulin <felix.tscheulin@flowd.de>
2018-09-05 11:47:45 +0200

    Use short array syntax, prevent php notice, correct typo

--- Classes/ErrorHandlers/SentryPageNotFoundHandler.php
+++ Classes/ErrorHandlers/SentryPageNotFoundHandler.php
@@ -7,16 +7,16 @@ use Raven_Client;
 class SentryPageNotFoundHandler {
 
     /**
-     * Log an message to Sentry and just continue with the normal flow in TSFE
+     * Log a message to Sentry and just continue with the normal flow in TSFE
      *
      * @param array $params
      * @param \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController $tsfe
      */
     public function handle($params, $tsfe) {
-        $ravenClient = $GLOBALS['SENTRY_CLIENT'];
+        $ravenClient = $GLOBALS['SENTRY_CLIENT'] ?? null;
 
         if($ravenClient) {
-            $ravenClient->message('404 Page not found', array(), Raven_Client::WARNING, false, $params);
+            $ravenClient->message('404 Page not found', [], Raven_Client::WARNING, false, $params);
         }
 
         $tsfe->pageErrorHandler($GLOBALS['TYPO3_CONF_VARS']['FE']['pageNotFound_handling_original']);
